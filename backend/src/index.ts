4b8ee7de7207de1a8e7d3c9cdf3d2420151f0abe
Kacper Nadstoga <kapix007@gmail.com>
2023-09-22 15:28:07 +0200

    prepare setup configuration for db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello world')
+import http from 'http'
+import { app } from './app'
+import { connect } from './config/database'
+
+const server = http.createServer(app)
+
+server.listen(process.env.API_PORT, async () => {
+    console.log(
+        `Hello running server from http://localhost:${process.env.API_PORT}`
+    )
+    await connect()
+})
