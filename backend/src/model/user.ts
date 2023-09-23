273ff37c934265576e0be17e5d14ec763a4e0b4e
Kacper Nadstoga <kapix007@gmail.com>
2023-09-23 12:53:51 +0200

    instal typesript types for mongoose and rules for eslint (backend) and create user interface, schema and model

---
+++ backend/src/model/user.ts
@@ -0,0 +1,27 @@
+import { IUser } from '../types/IUser'
+import { Schema, model } from 'mongoose'
+
+const userSchema = new Schema<IUser>({
+    name: {
+        type: String,
+        required: true,
+    },
+    surname: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    token: {
+        type: String,
+        required: true,
+    },
+})
+
+export const User = model<IUser>('User', userSchema)
