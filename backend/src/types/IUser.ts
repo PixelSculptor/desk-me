22835a74fecab41a1ad3e9fc7f394a8fddc3ab0c
Kacper Nadstoga <kapix007@gmail.com>
2023-09-23 12:55:40 +0200

    update prettier rules

--- backend/src/types/IUser.ts
+++ backend/src/types/IUser.ts
@@ -1,7 +1,7 @@
 export interface IUser {
-    email: string
-    name: string
-    surname: string
-    password: string
-    token: string
+    email: string;
+    name: string;
+    surname: string;
+    password: string;
+    token: string;
 }
