fc482807f168c133d9757495518cad4b0c359737
Kacper Nadstoga <52595886+PixelSculptor@users.noreply.github.com>
2023-09-26 14:35:58 +0200

    Registration (#3)
    * move frontend layer to frontend directory
    
    * setup backend layer
    
    * format package.json
    
    * create basic folder structure for backend code base
    
    * prepare setup configuration for db
    
    * update .gitignore
    
    * instal typesript types for mongoose and rules for eslint (backend) and create user interface, schema and model
    
    * update prettier rules
    
    * update prettier rules
    
    * create user model, custom request interface user model interface and basic user registration endpoint and .env sample file
    
    * refactor register endpoint and move functions to utils controllers and router directories
    
    * create custom error mapped type and custom response interface
    
    * create custom error handling and custom response object

--- backend/src/types/CustomResponse.ts
+++ backend/src/types/CustomResponse.ts
@@ -1,5 +1,19 @@
 import { Response } from 'express';
+import { HttpClientStatusErrorCodes, CustomError } from '../utils/CustomError';
 
-export interface CustomResponse<T, K> extends Response {
-    response: T | CustomEvent<K>;
+type HttpSuccessStatusCodes = 200 | 201 | 202 | 204;
+
+type CustomPayload<T> = {
+    code: HttpSuccessStatusCodes;
+    body: T;
+};
+export interface CustomResponse<T> extends Response {
+    // eslint-disable-next-line
+    status: (
+        status: HttpSuccessStatusCodes | HttpClientStatusErrorCodes
+    ) => this;
+    // eslint-disable-next-line
+    send: (
+        body: CustomPayload<T> | CustomError<HttpClientStatusErrorCodes>
+    ) => this;
 }
