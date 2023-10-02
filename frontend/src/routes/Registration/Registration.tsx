800c117b6d98eef6c3e496a8d8c602eadc3c865c
Kacper Nadstoga <kapix007@gmail.com>
2023-10-02 11:25:40 +0200

    create mixin for flexbox and add basic styles for positioning and styling registration form

--- frontend/src/routes/Registration/Registration.tsx
+++ frontend/src/routes/Registration/Registration.tsx
@@ -3,16 +3,15 @@ import { Link } from 'react-router-dom';
 import { RegistrationForm } from '@components/RegistrationForm/RegistrationForm';
 
 import styles from './Registration.module.scss';
-import classNames from 'classnames';
 
 export function Registration() {
     return (
-        <section className={classNames(styles['registrationPanel'])}>
-            <h2 className="registrationPanel__heading">
+        <section className={styles['registrationPanel']}>
+            <h2 className={styles['registrationPanel__heading']}>
                 Stwórz swoje konto już dzisiaj
             </h2>
             <RegistrationForm />
-            <h6 className="registrationPanel__redirectLogin">
+            <h6 className={styles['registrationPanel__redirectLogin']}>
                 Masz już konto? <Link to={'/login'}>Zaloguj się</Link>
             </h6>
         </section>
