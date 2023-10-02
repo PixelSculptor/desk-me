2794853f3257f094d9a6779129b079f0aa4eff48
Kacper Nadstoga <kapix007@gmail.com>
2023-10-02 09:27:49 +0200

    create type scale typography

--- frontend/src/routes/Registration/Registration.tsx
+++ frontend/src/routes/Registration/Registration.tsx
@@ -2,9 +2,12 @@ import { Link } from 'react-router-dom';
 
 import { RegistrationForm } from '@components/RegistrationForm/RegistrationForm';
 
+import styles from './Registration.module.scss';
+import classNames from 'classnames';
+
 export function Registration() {
     return (
-        <section className="registrationPanel">
+        <section className={classNames(styles['registrationPanel'])}>
             <h2 className="registrationPanel__heading">
                 Stwórz swoje konto już dzisiaj
             </h2>
