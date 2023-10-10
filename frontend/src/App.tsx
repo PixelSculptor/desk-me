f984b9437e8459f689f06c62de61ec4126015e5f
Kacper Nadstoga <kapix007@gmail.com>
2023-10-10 22:28:50 +0200

    add transition animation with framer motion lib

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,21 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+
 import { Home } from '@routes/Home/Home';
 import { Registration } from '@routes/Registration/Registration';
 import { ROUTES } from './types/Routes';
 import { Login } from './routes/Login/Login';
 
 function App() {
+    const location = useLocation();
     return (
-        <>
-            <Routes>
+        <AnimatePresence>
+            <Routes location={location} key={location.pathname}>
                 <Route path={ROUTES.Home} element={<Home />} />
                 <Route path={ROUTES.Register} element={<Registration />} />
                 <Route path={ROUTES.Login} element={<Login />} />
             </Routes>
-        </>
+        </AnimatePresence>
     );
 }
 
--- frontend/src/routes/Login/Login.tsx
+++ frontend/src/routes/Login/Login.tsx
@@ -1,11 +1,21 @@
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 import styles from './Login.module.scss';
 import { LoginForm } from '@/components/LoginForm/LoginForm';
 
 export function Login() {
     return (
-        <section className={styles['loginPanel']}>
+        <motion.section
+            className={styles['loginPanel']}
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+                duration: 0.8,
+                delay: 0.2,
+                ease: [0, 0.71, 0.2, 1.01],
+            }}
+        >
             <h2 className={styles['loginPanel__heading']}>
                 Zaloguj się do swojego konta
             </h2>
@@ -14,6 +24,6 @@ export function Login() {
                 Nie masz jeszcze konta?
                 <Link to={'/register'}> Zarejestruj&nbsp;się</Link>
             </h6>
-        </section>
+        </motion.section>
     );
 }
