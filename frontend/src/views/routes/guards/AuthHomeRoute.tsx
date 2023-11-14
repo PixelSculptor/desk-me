272a6d6c85ee1e548fdfaeb9af8259a67058fcd9
Kacper Nadstoga <kapix007@gmail.com>
2023-11-14 08:00:10 +0100

    change name directory for route guards

--- frontend/src/views/routes/guards/AuthHomeRoute.tsx
+++ frontend/src/views/routes/guards/AuthHomeRoute.tsx
@@ -1,5 +1,5 @@
 import { Home } from '@/views/Home/Home';
 
-import { AuthRoute } from '@/views/guards/ConditionRoute/ConditionRoute';
+import { AuthRoute } from '@/views/routes/ConditionRoute/ConditionRoute';
 
 export const AuthHome = AuthRoute<unknown>(Home);
