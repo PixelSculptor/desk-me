85837545cef75f2d85c8d203593b76563a400774
Kacper Nadstoga <kapix007@gmail.com>
2023-11-14 08:00:27 +0100

    fix import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import { AnimatePresence } from 'framer-motion';
 import { Registration } from '@/views/Registration/Registration';
 import { ROUTES } from './types/Routes';
 import { Login } from '@/views/Login/Login';
-import { AuthHome } from '@/views/routes/Auth/AuthHomeRoute';
+import { AuthHome } from '@/views/routes/guards/AuthHomeRoute';
 
 function App() {
     const location = useLocation();
