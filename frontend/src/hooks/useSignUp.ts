8cbfb44daa5b7d0a413eab5e36d2dbf0b4df8c03
Kacper Nadstoga <52595886+PixelSculptor@users.noreply.github.com>
2023-11-27 09:00:57 +0100

    create custom hook for checking authentication user in redux store (#19)

--- frontend/src/hooks/useSignUp.ts
+++
@@ -1,6 +0,0 @@
-import { UserModelTypes } from '../types/UserTypes';
-/* eslint-disable @typescript-eslint/no-unused-vars */
-export const UseSignUp = async () // userCredentials: UserCredentialTypes
-: Promise<UserModelTypes | undefined> => {
-    return undefined;
-};
--- frontend/src/views/routes/guards/ProtectedRoute.tsx
+++ frontend/src/views/routes/guards/ProtectedRoute.tsx
@@ -1,11 +1,10 @@
 import { Navigate, useLocation } from 'react-router-dom';
-import { isAuth } from '@/hooks/isAuth';
-import { useAppSelector } from '@/store/store';
-import { selectUser } from '@/store/user/user.selector';
+
 import { ROUTES } from '@/types/Routes';
+import { useAuth } from '@/hooks/useAuth';
 
 export const ProtectedRoute: React.FC<{ children: JSX.Element }> = ({ children }: { children: JSX.Element }) => {
-    const isAuthenticated = isAuth(useAppSelector(selectUser));
+    const { isAuthenticated } = useAuth();
     const location = useLocation();
 
     if (!isAuthenticated) {
