cd8d9e452359de263a8c7ce839f1edcfff31c3f7
Kacper Nadstoga <kapix007@gmail.com>
2023-12-10 23:11:35 +0100

    create styles for fallback page and add link to back to home page

--- frontend/src/views/NotFound/NotFound.tsx
+++ frontend/src/views/NotFound/NotFound.tsx
@@ -1,5 +1,17 @@
+import { Link } from 'react-router-dom';
+
+import styles from './NotFound.module.scss';
+
 function NotFound() {
-    return <div>NotFound</div>;
+    return (
+        <div className={styles['notFoundView']}>
+            <h2 className={styles['notFoundView__headerAlert']}>Nie znaleziono strony</h2>
+            <h3 className={styles['notFoundView__errorCode']}>Kod błędu: 404</h3>
+            <Link className={styles['notFoundView__fallbackLink']} to="/">
+                Wróć do strony domowej
+            </Link>
+        </div>
+    );
 }
 
 export default NotFound;
