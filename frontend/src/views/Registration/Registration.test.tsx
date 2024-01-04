74987a4ce8ec46c6e2fa0cc8a73d9c6c2e34535c
Kacper Nadstoga <kapix007@gmail.com>
2024-01-04 08:10:19 +0100

    fix unit test testing navigating to home page

--- frontend/src/views/Registration/Registration.test.tsx
+++ frontend/src/views/Registration/Registration.test.tsx
@@ -1,18 +1,18 @@
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import userEvent from '@testing-library/user-event';
 
 import { TSignUpSchema } from '@/components/RegistrationForm/RegistrationForm.types';
 import { store } from '@store/store';
-import { Registration } from './Registration';
+import App from '@/App';
 
 const MockRegistration = () => {
     return (
         <Provider store={store}>
-            <BrowserRouter>
-                <Registration />
-            </BrowserRouter>
+            <MemoryRouter initialEntries={['/register']}>
+                <App />
+            </MemoryRouter>
         </Provider>
     );
 };
@@ -31,15 +31,19 @@ describe('Registration Panel Tests', () => {
 
         const header = screen.getByText(/Stwórz swoje konto już dzisiaj/i);
         const inviteToLoginHeader = screen.getByText(/Masz już konto?/);
+
         expect(header).toBeInTheDocument();
         expect(inviteToLoginHeader).toBeInTheDocument();
     });
     it("Should navigate to login panel when user clicks to router link with phrase 'Masz już konto? Zaloguj się'", async () => {
         render(<MockRegistration />);
         const redirectLink = screen.getByText(/Zaloguj się/i);
+
         fireEvent.click(redirectLink);
-        await waitFor(() => {
-            expect(window.location.pathname).toBe('/login');
+
+        waitFor(() => {
+            const heading = screen.findByRole('heading', { level: 2 });
+            expect(heading).toHaveTextContent('Zaloguj się do swojego konta');
         });
     });
     it('Should move to dashboard view after proper credentials during registration without any error messages below register form inputs', async () => {
@@ -51,19 +55,19 @@ describe('Registration Panel Tests', () => {
         const password = screen.getByLabelText('Hasło');
         const confirmPassword = screen.getByLabelText('Powtórz hasło');
         const submit = screen.getByRole('button');
-        expect(submit).toHaveTextContent(/Zarejestruj się/i);
 
         userEvent.type(name, CORRECT_USER.name);
         userEvent.type(surname, CORRECT_USER.surname);
         userEvent.type(email, CORRECT_USER.email);
         userEvent.type(password, CORRECT_USER.password);
         userEvent.type(confirmPassword, CORRECT_USER.confirmPassword);
-        await userEvent.click(submit);
 
-        await waitFor(() => {
-            const heading = screen.findByRole('heading', { level: 2 });
+        fireEvent.click(submit);
+
+        waitFor(() => {
             expect(window.location.pathname).toBe('/');
-            expect(heading).toHaveTextContent('Witaj');
+            const heading = screen.findByRole('heading', { level: 2 });
+            expect(heading).toHaveTextContent('Witaj Adam');
         });
     });
 });
