339a8953f344f223eb1d6f7082241685c4b66567
Kacper Nadstoga <kapix007@gmail.com>
2024-02-13 15:54:26 +0100

    Write unit test for checking greeting on Home View

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -3,17 +3,12 @@ import { http, HttpResponse } from 'msw';
 
 import { UserResponse } from '@/types/UserTypes';
 
+import { MOCK_USER } from './mockData';
+
 const API_URL = import.meta.env.VITE_API_URL as string;
 
 export const handlers = [
     http.post(`${API_URL}/auth/login`, async () => {
-        return HttpResponse.json<UserResponse>({
-            id: '1',
-            email: 'joe-doe@gmail.com',
-            name: 'John',
-            surname: 'Doe',
-            accessToken: '0913ufknjw9j834f',
-            refreshToken: '0913ufkd32jw9j834f',
-        });
+        return HttpResponse.json<UserResponse>(MOCK_USER);
     }),
 ];
---
+++ frontend/src/mocks/mockData.ts
@@ -0,0 +1,10 @@
+import { UserResponse } from '@/types/UserTypes';
+
+export const MOCK_USER: UserResponse = {
+    id: '1',
+    email: 'joe-doe@gmail.com',
+    name: 'John',
+    surname: 'Doe',
+    accessToken: '0913ufknjw9j834f',
+    refreshToken: '0913ufkd32jw9j834f',
+};
